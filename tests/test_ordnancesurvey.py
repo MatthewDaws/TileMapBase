45fb61031287810c996701430a038d2c891f2021
Matthew Daws <matt.daws@cantab.net>
2017-07-30 17:14:09 +0100

    Ordnance Survery projections

--- tests/test_ordnancesurvey.py
+++ tests/test_ordnancesurvey.py
@@ -1,10 +1,42 @@
 import pytest
+import unittest.mock as mock
 
 import tilemapbase.ordnancesurvery as ons
+import os
 
 def test_project():
     assert ons.project(-1.55532, 53.80474) == pytest.approx((429383.15535285, 434363.0962841))
     
 def test_to_os_national_grid():
-    assert ons.to_os_national_grid(-1.55532, 53.80474) == ("SE", None, None)
-    
+    assert ons.to_os_national_grid(-1.55532, 53.80474) == ("SE 29383 34363",
+        pytest.approx(0.155352845), pytest.approx(0.096284069))
+    assert ons.to_os_national_grid(-5.71808, 50.06942) == ("SW 34041 25435",
+        pytest.approx(0.0757940984), pytest.approx(0.90742218543))
+    assert ons.to_os_national_grid(-3.02516, 58.64389) == ("ND 40594 73345",
+        pytest.approx(0.4899132418), pytest.approx(0.118179377))
+
+def our_os_path():
+    with mock.patch("os.path") as mock_path:
+        def our_abspath(x):
+            print(x)
+            raise Exception()
+        mock_path.abspath = our_abspath
+        yield mock_path
+
+def our_os_scandir():
+    with mock.patch("os.scandir") as mock_scan:
+        def our_scan(x):
+            print(x)
+            raise Exception()
+        mock_scan.side_effect = our_scan
+        yield mock_scan
+
+def test_init():
+    ons.init(os.path.join("tests", "test_os_map_data"))
+    base = os.path.abspath(os.path.join("tests", "test_os_map_data", "data"))
+
+    assert ons._openmap_local_lookup == {
+        "AH" : os.path.join(base, "one"),
+        "AA" : os.path.join(base, "two") }
+    assert ons._vectormap_local_lookup == {
+        "BG" : os.path.join(base, "one") }
--- tilemapbase/ordnancesurvery.py
+++ tilemapbase/ordnancesurvery.py
@@ -2,12 +2,59 @@
 ordnancesurvery
 ~~~~~~~
 
-Supports raster tiles from the Ordnance Survey
+Supports raster tiles from the Ordnance Survey.  We support the (freely
+downloadable) "OS OpenMap Local" and "OS VectorMap Local".
+
+- "OS OpenMap Local" has file-names like "SE00NE.tif" where "SE" is the "grid
+  code", "00" is the first digit of the x and y grid reference, and "NE" means
+  "North East" i.e. the upper left part of the "00" grid
+- "OS VectorMap Local" has file-name like "SE00.tif" with the same format, but
+  2 times less resolution in both coorindates.
 
 """
 
 import math as _math
+import os as _os
+import re as _re
 import PIL.Image as _Image
+
+# Singletons
+_openmap_local_lookup = None
+_vectormap_local_lookup = None
+
+def init(start_directory):
+    """Perform a search for tile files, and so initialise the support.
+
+    :param start_directory: The string name of the directory to search.  May
+      also be an iterable of strings to search more than one directory.  All
+      sub-directories will be searched for valid filenames.
+    """
+    global _openmap_local_lookup, _vectormap_local_lookup
+    _openmap_local_lookup = dict()
+    _vectormap_local_lookup = dict()
+    if not isinstance(start_directory, str):
+        dirs = list(start_directory)
+    else:
+        dirs = [start_directory]
+    while len(dirs) > 0:
+        dirname = dirs.pop()
+        dirs.extend(_init_scan_one_directory(dirname))
+
+def _init_scan_one_directory(dir_name):
+    global _openmap_local_lookup, _vectormap_local_lookup
+    oml = _re.compile("^[A-Z]{2}\d\d[NESW]{2}\.tif$")
+    vml = _re.compile("^[A-Z]{2}\d\d.tif$")
+    dirs = []
+    dir_name = _os.path.abspath(dir_name)
+    for entry in _os.scandir(dir_name):
+        if entry.is_dir():
+            dirs.append(_os.path.abspath(entry.path))
+        elif entry.is_file():
+            if oml.match(entry.name):
+                _openmap_local_lookup[entry.name[:2]] = dir_name
+            elif vml.match(entry.name):
+                _vectormap_local_lookup[entry.name[:2]] = dir_name
+    return dirs
 
 def to_os_national_grid(longitude, latitude):
     """Converts the longitude and latitude coordinates to the Ordnance Survery
@@ -15,6 +62,11 @@ def to_os_national_grid(longitude, latitude):
     
     :return: `(grid_code, eastings, northings)`
     """
+    grid_code, x, y = _code_grid_residual(longitude, latitude)
+    xx, yy = _math.floor(x), _math.floor(y)
+    return "{} {} {}".format(grid_code, xx, yy), x - xx, y - yy
+
+def _code_grid_residual(longitude, latitude):
     codes = "ABCDEFGHJKLMNOPQRSTUVWXYZ"
     x, y = project(longitude, latitude)
     
@@ -24,14 +76,16 @@ def to_os_national_grid(longitude, latitude):
         raise ValueError()
     grid_code = codes[index]
     
-    x100, y100 = x - 500000 * x500, y - 500000 * y500
-    x100, y100 = _math.floor(x100 / 100000), _math.floor(y100 / 100000)
+    x, y = x - 500000 * x500, y - 500000 * y500
+    x100, y100 = _math.floor(x / 100000), _math.floor(y / 100000)
     index = x100 + (4 - y100) * 5
     if index < 0 or index >= 25:
         raise AssertionError()
     grid_code += codes[index]
 
-    return grid_code, None, None
+    return grid_code, x - x100 * 100000, y - y100 * 100000
+
+
 
 
 ##### (Optional) usage of pyproj
